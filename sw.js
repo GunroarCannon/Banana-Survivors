127ac86b51a6719ac1f8ea927a789a976ca0313c
GunroarCannon <gunroarcannon@gmail.com>
2026-03-30 10:17:45 +0100

    PWA stuff

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'banana-survivors-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/icon.png',
+  '/assets/banana.png',
+  '/scene.fx.js',
+  '/noise.js',
+  '/config.js',
+  '/base.js',
+  '/abilities.js',
+  '/player.js',
+  '/enemies.js',
+  '/ui.js',
+  '/scenes.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/phaser/3.60.0/phaser.min.js',
+  'https://fonts.googleapis.com/css2?family=Fredoka+One&family=Nunito:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
